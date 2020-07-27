d3aaed257df74fc41f0f2b5faaf12216857bdcf1
young-seong ok <okys2010@gmail.com>
2020-07-27 22:22:29 +0900

    button 테스트

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import TailwindCss from "tailwindcss";
 import "./styles/style.scss";
 import VueCompositionApi from "@vue/composition-api";
 
-Vue.use(TailwindCss);
 Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
 
